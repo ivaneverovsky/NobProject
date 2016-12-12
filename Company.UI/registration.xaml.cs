8777ed48e58050e712175dba5d9b77a78851b516
MosyakovSergey <s.v.mosyakov@gmail.com>
2016-12-12 18:54:38 +0300

    registration logic and design was added

--- Company.UI/registration.xaml.cs
+++ Company.UI/registration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Company.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,9 @@ namespace Company.UI
     /// </summary>
     public partial class registration : Window
     {
+
+        List<string> ListNewUser = new List<string>();
+
         public registration()
         {
             InitializeComponent();
@@ -26,12 +30,49 @@ namespace Company.UI
 
         private void enter_Click(object sender, RoutedEventArgs e)
         {
-            //при нажатии данные отправляются в бд и записываются
+            var a = name.ToString();
+            ListNewUser.Add(a);
 
+            var b = surname.ToString();
+            ListNewUser.Add(b);
+
+            var c = login.ToString();
+            ListNewUser.Add(c);
+
+            var d = password.ToString();
+            ListNewUser.Add(d);
+
+            //Разделяю данные в ListOrders
+            using (var g = new Context())
+            {
+                foreach (string item in ListNewUser)
+                {
+                    //отделяю элементы
+                    string[] f = item.Split(' ');
+                    //int price = Convert.ToInt32(f[1]);
+                    //var itemname = new Catalogue { ItemName = f[0] };
+
+                    g.Orders.Add(new Orders
+                    {
+                        //ItemName = itemname,
+                        //Cost = price
+                    });
+                    g.SaveChanges();
+                }
+                MessageBox.Show("Пользователь сохранен");
+                ListNewUser.Clear();
+            }
+
+            //при нажатии данные отправляются в бд и записываются
             //возвращение к окну входа(логин), после записи данных в бд!
             login log = new login();
             Close();
             log.ShowDialog();
+        }
+
+        private void type_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
