c56f01e99ed1524018891d58ecbb56b5210719df
Ivsereda97 <Ivsereda97@gmail.com>
2016-12-15 00:12:30 +0300

    Показывает нейм и сернейм в каждом поле

--- Company.UI/login.xaml.cs
+++ Company.UI/login.xaml.cs
@@ -39,8 +39,55 @@ namespace Company.UI
             //repo.AuthorizationAdmin();
             //repo.AuthorizationAdmin();
             //repo.AuthorizationSupplier();
-           
-                Dictionary<string, string> dictAuthClient = new Dictionary<string, string>();
+            Dictionary<string, string> dictSuppliersSurname = new Dictionary<string, string>();
+            using (var c = new Context())
+            {
+                foreach (var field in c.Suppliers)
+                {
+                    dictSuppliersSurname.Add(field.login, field.Name);
+                }
+            }
+            Dictionary<string, string> dictSuppliersName = new Dictionary<string, string>();
+            using (var c = new Context())
+            {
+                foreach (var field in c.Suppliers)
+                {
+                    dictSuppliersName.Add(field.login, field.Surname);
+                }
+            }
+            Dictionary<string, string> dictAdminsName = new Dictionary<string, string>();
+            using (var c = new Context())
+            {
+                foreach (var field in c.Admins)
+                {
+                    dictAdminsName.Add(field.login, field.Name);
+                }
+            }
+            Dictionary<string, string> dictAdminsSurname = new Dictionary<string, string>();
+            using (var c = new Context())
+            {
+                foreach (var field in c.Admins)
+                {
+                    dictAdminsSurname.Add(field.login, field.Surname);
+                }
+            }
+            Dictionary<string, string> dictClientsName = new Dictionary<string, string>();
+            using (var c = new Context())
+            {
+                foreach (var field in c.Clients)
+                {
+                    dictClientsName.Add(field.login, field.Name);
+                }
+            }
+            Dictionary<string, string> dictClientsSurname = new Dictionary<string, string>();
+            using (var c = new Context())
+            {
+                foreach (var field in c.Clients)
+                {
+                    dictClientsSurname.Add(field.login, field.Surname);
+                }
+            }
+            Dictionary<string, string> dictAuthClient = new Dictionary<string, string>();
             using (var c = new Context())
             {
                 foreach (var login in c.Clients)
@@ -65,18 +112,24 @@ namespace Company.UI
                 }
             }
             string test;
-            
 
 
 
 
 
+            string ClientName;
+            string ClientSurname;
             //добавить, после настройки пункта enter_Click
             if (dictAuthClient.TryGetValue(loginBox.Text,out test))
-            {
-               
-                
-                     ;
+            { 
+                foreach (var item in dictClientsName)
+                {
+                    if (dictClientsName.TryGetValue(loginBox.Text, out ClientName) && dictClientsSurname.TryGetValue(loginBox.Text, out ClientSurname))
+                    {
+                        client.ClientNameBox.Text = ClientName;
+                        client.ClientSurnameNameBox.Text = ClientSurname;
+                    }
+                }
                   
                 
                 Close();
@@ -84,16 +137,31 @@ namespace Company.UI
 
                 return; 
                 }
-            
+
+            string AdminName;
+            string AdminSurname;
             if (dictAuthAdmin.TryGetValue(loginBox.Text,out test))
             {
+                if (dictAdminsName.TryGetValue(loginBox.Text, out AdminName) && dictAdminsSurname.TryGetValue(loginBox.Text, out AdminSurname))
+                {
+                    admin.AdminNameBox.Text = AdminName;
+                    admin.AdminSurnameBox.Text = AdminSurname;
+                }
+
                 Close();
                 admin.ShowDialog();
                 return;
                 
             }
+            string SupplierName;
+            string SupplierSurname;
             if (dictAuthSupplier.TryGetValue(loginBox.Text,out test))
             {
+                if (dictSuppliersName.TryGetValue(loginBox.Text, out SupplierName) && dictSuppliersSurname.TryGetValue(loginBox.Text, out SupplierSurname))
+                {
+                    supplier.SupplierNameBox.Text = SupplierName;
+                    supplier.SupplierSurnameBox.Text = SupplierSurname;
+                }
                 Close();
                 supplier.ShowDialog();
                 return;
